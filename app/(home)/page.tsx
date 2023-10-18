263e3b2ddad3ca303aee29777e2b0971f04d57e2
Eduardo Santos <52334473+SkDudu@users.noreply.github.com>
2023-10-18 12:11:33 -0300

    feat: add images in high quality

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,6 +6,7 @@ import CategoriesMenu from './components/categoriesMenu'
 import ProductList from './components/productList'
 
 import banner01 from "../../public/banner01.png"
+import banner02 from "../../public/banner02.png"
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -32,8 +33,18 @@ export default async function Home() {
       </div>
 
       <div className='mt-8'>
+        <h1 className='font-bold uppercase mb-2'>Ofertas</h1>
         <ProductList products={deals}/>
       </div>
+
+      <Image 
+        src={banner02} 
+        alt={'Até 55% de desconto em mouses.'}
+        width={0}
+        height={0}
+        className='h-auto w-full'
+        sizes='100vw'
+      />
     </div>
   )
 }
