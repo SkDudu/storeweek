6ad156159c7bf25e6a9803c2046976bfc4c43eda
Eduardo Santos <52334473+SkDudu@users.noreply.github.com>
2023-10-18 11:39:19 -0300

    core: add s3 domain to next config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['www.google.com']
+        domains: ['www.google.com', 'fsw-store.s3.sa-east-1.amazonaws.com']
     }
 }
 
