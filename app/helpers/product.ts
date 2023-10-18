78c7b128edec0f69e309c14cff9f01f32aa32353
Eduardo Santos <52334473+SkDudu@users.noreply.github.com>
2023-10-18 11:39:42 -0300

    feat: function do sum price and discount

---
+++ app/helpers/product.ts
@@ -0,0 +1,17 @@
+import { Product } from "@prisma/client";
+
+interface ProductWithTotalPrice extends Product{
+    totalPrice: number
+}
+
+export const computProductsTotalPrice = (product: Product): ProductWithTotalPrice => {
+    if(product.discount === 0){
+        return{...product, totalPrice: Number(product.baseprice)}
+    }
+
+    const totalPrice = Number(product.baseprice) * (product.discount) / 100
+
+    return{
+        ...product, totalPrice
+    }
+}
