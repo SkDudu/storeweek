d8ac43e2030c4eb3fa349299a1ff0b0cf212979f
Eduardo Santos <52334473+SkDudu@users.noreply.github.com>
2023-10-18 15:53:37 -0300

    chore: add category items

---
+++ app/catalog/components/categoryItem.tsx
@@ -0,0 +1,31 @@
+import { Category } from "@prisma/client";
+import Image from "next/image";
+
+interface CategoryItemProps {
+    category: Category
+}
+
+const CategoryItem = ({category}: CategoryItemProps) => {
+    return (
+        <div className="flex flex-col">
+            <div className="w-full h-[150px] flex items-center justify-center rounded-tl-lg rounded-tr-lg bg-gradient-to-t from-purple-800 to-purple-950">
+                <Image 
+                    src={category.imageUrl}
+                    alt={category.name}
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    className="h-auto max-h-[70%] w-auto max-w-[80%]"
+                    style={{
+                        objectFit: "contain"
+                    }}
+                />
+            </div>
+            <div className="bg-accent py-2 rounded-br-lg rounded-bl-lg text-center">
+                <p className="text-sm font-semibold">{category.name}</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default CategoryItem;
--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,6 +1,20 @@
-const CatalogPage = () => {
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/categoryItem";
+
+const CatalogPage = async() => {
+    const categories = await prismaClient.category.findMany({})
     return (
-        <p>Catálogo</p>
+        <div className="p-5">
+            <Badge variant={"outline"} className="gap-1 border-primary text-base uppercase py-1 border-2">
+                <ShapesIcon size={16}/>
+                Catálogo
+            </Badge>
+            <div className="grid grid-cols-2 gap-8 mt-4">
+                {categories.map(category => <CategoryItem key={category.id} category={category}/>)}
+            </div>
+        </div>
     );
 }
  
